20c4e196e338adcde2b3350d366c57dad36f8c13
brennop <brennopcordeiro@gmail.com>
2020-11-14 20:35:36 -0300

    🤦 Esqueci a pasta bin

--- node-express/routes/users.js
+++ node-express/routes/users.js
@@ -4,9 +4,10 @@ const User = require("../models").User;
 
 /* GET users listing. */
 /* index */
-router.get("/", async function (req, res, next) {
+router.get("/:format?", async function (req, res, next) {
   const users = await User.findAll();
-  res.render("../views/users/index", { users });
+  if (req.params.format) res.json(users);
+  else res.render("../views/users/index", { users });
 });
 
 module.exports = router;
