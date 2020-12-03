e19f04aad8ee287739b9f260ca6f349968bbee5e
brennop <brennopcordeiro@gmail.com>
2020-12-03 18:40:47 -0300

    deixa html do node mais parecido com o do rails

--- node-express/routes/index.js
+++ node-express/routes/index.js
@@ -9,15 +9,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get("/users/new",function (req, res) {
-  res.render("../views/users/new",{ action:"users/show" });
- });
-
-
-router.get("/users/:userId", async function (req, res, next) {
-  const {userId}= req.params;
-  const user = await User.findOne({where:{id:userId}});
-  if (req.params.format) res.json(user);
-  else res.render("../views/users/edit", { user: user, action:userId });
-})
 module.exports = router;
--- node-express/routes/users.js
+++ node-express/routes/users.js
@@ -1,60 +1,66 @@
 var express = require("express");
 var router = express.Router();
 const User = require("../models").User;
-const bodyParser= require('body-parser')
+const bodyParser = require("body-parser");
 
-router.use(bodyParser.urlencoded({extended:true}))
+router.use(bodyParser.urlencoded({ extended: true }));
 
-
-
-
-
- router.post("/users/show",async(req, res) =>{
-  try{
-
-    const newUser= await User.create(req.body)
+router.post("/", async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
     return res.status(201).json({
       newUser,
-    })
-
-  } catch(error){
-    console.log(req.body)
-      return res.status(500).json({error: error})
+    });
+  } catch (error) {
+    console.log(req.body);
+    return res.status(500).json({ error: error });
   }
 });
 
-
-router.put("/users/:userId", async(req, res)=>{
-  try{
-    const {userId}= req.params;
-    const [updated]= await User.update(req.body,{
-      where: {id: userId}
-    })
-    if(updated){
-      const updatedUser = await User.findOne({where:{id:userId}})
-      return res.status(200).json({user: updatedUser});
+router.put("/users/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const [updated] = await User.update(req.body, {
+      where: { id: userId },
+    });
+    if (updated) {
+      const updatedUser = await User.findOne({ where: { id: userId } });
+      return res.status(200).json({ user: updatedUser });
     }
-    throw new Error('User not found');
-
-  }catch(error){
-    return res.status(500).json({error: error})
+    throw new Error("User not found");
+  } catch (error) {
+    return res.status(500).json({ error: error });
   }
-})
+});
 
 // router.post("/users/show",async(req, res) =>{
 //   console.log(req.body)
 // });
 
+/* GET users listing. */
 
- /* GET users listing. */
+/* index */
 
- /* index */
-
- router.get("/:format?", async function (req, res, next) {
+router.get("/", async function (req, res, next) {
   const users = await User.findAll();
-  if (req.params.format) res.json(users);
-  else res.render("../views/users/index", { users });
+  res.render("../views/users/index", { users });
 });
 
+router.get("/json", async function (req, res, next) {
+  const users = await User.findAll();
+  res.json(users);
+});
+
+router.get("/new",function (req, res) {
+  res.render("../views/users/new",{ action:"users/show" });
+ });
+
+/*
+router.get("/:userId", async function (req, res, next) {
+  const {userId}= req.params;
+  const user = await User.findOne({where:{id:userId}});
+  if (req.params.format) res.json(user);
+  else res.render("../views/users/edit", { user: user, action:userId });
+})*/
 
 module.exports = router;
